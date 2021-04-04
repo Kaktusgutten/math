bfa34723ae764010956895b958d2415f9425310a
Kaktusgutten <81820769+Kaktusgutten@users.noreply.github.com>
2021-04-04 14:46:41 +0200

    Update Oblig6_matematikk.py

--- Oblig6_matematikk.py
+++ Oblig6_matematikk.py
@@ -23,7 +23,7 @@ a12 = float(input("tall for rad: 1. kollonne: 2, = "))
 a21 = float(input("tall for rad: 2, kollonne: 1, = "))
 a22 = float(input("tall for rad: 2, kollonne: 2, = "))
 
-def determinat(A_mat):
+def determinant(A_mat):
     A_det = A_mat[0][0]*A_mat[1][1] - A_mat[1][0]*A_mat[0][1]
     #print(A_det)
     return A_det
@@ -31,13 +31,13 @@ def determinat(A_mat):
 A_matrise = np.array([[a11, a12], [a21, a22]])
 A_adjungert_matrise = np.array([[a22, -a12], [-a21, a11]])
 
-#print(determinat(A_matrise))
-#print("Determinaten =", determinat(A_matrise))
+#print(determinant(A_matrise))
+#print("Determinanten =", determinant(A_matrise))
 
 
-if determinat(A_matrise) != 0:
+if determinant(A_matrise) != 0:
     print("Denne 2x2 matrisen kan inverteres")
-    Invers_matrise = (1/determinat(A_matrise))*A_adjungert_matrise
+    Invers_matrise = (1/determinant(A_matrise))*A_adjungert_matrise
     print("Inversen til A_matrise [A^-1]) = ", Invers_matrise)
     
     #Kontroll av inversen:
@@ -62,76 +62,31 @@ if determinat(A_matrise) != 0:
     Kontroll_av_funn_av_InversMatrise = np.asarray(C_matrise_tuple)
     print("Dette er kontroll av inversen til matrisen = ", Kontroll_av_funn_av_InversMatrise)
 else:
-    print("Determinant = ", determinat(A_matrise), "Og kan derfor ikke inverteres")
+    print("Determinant = ", determinant(A_matrise), "Og kan derfor ikke inverteres")
 """
 
 # Oppg 2) Determinerer 3x3 Matriser
 """
-
-#Endre Vilkårlig matrise
-
 import numpy as np
-
-#Rad 1
-a11 = float(input("tall for rad: 1, kollonne: 1, = "))
-a12 = float(input("tall for rad: 1. kollonne: 2, = "))
-a13 = float(input("tall for rad: 1, kollonne: 3, = "))
-
-#Rad 2
-a21 = float(input("tall for rad: 2, kollonne: 1, = "))
-a22 = float(input("tall for rad: 2. kollonne: 2, = "))
-a23 = float(input("tall for rad: 2, kollonne: 3, = "))
-
-#Rad 3
-a31 = float(input("tall for rad: 3, kollonne: 1, = "))
-a32 = float(input("tall for rad: 3. kollonne: 2, = "))
-a33 = float(input("tall for rad: 3, kollonne: 3, = "))
 
 def determinat(A_mat):
     M11 = A_mat[1][1]*A_mat[2][2] - A_mat[2][1]*A_mat[1][2]
     M12 = A_mat[1][0]*A_mat[2][2] - A_mat[2][0]*A_mat[1][2]
     M13 = A_mat[1][0]*A_mat[2][1] - A_mat[2][0]*A_mat[1][1]
     a_det = A_mat[0][0]*M11 - A_mat[0][1]*M12 + A_mat[0][2]*M13
-    #print(A_det)
+    #print(a_det)
     #print(M11)
     #print(M12)
     #print(M13)
     return a_det
 
-A_matrise = np.array([[a11, a12, a13], [a21, a22, a23], [a31, a32, a33]])
+Matrise_A = np.random.randint(-50, high=50, size=(3, 3), dtype='l')
 
-print("Determinaten til 3x3 matrise er: ", determinat(A_matrise))
+print("Determinanten til 3x3 matrise er: ", determinant(Matrise_A))
 """
-
 # Oppg 3) Matrise multiplisering: 2x3 * 3x2
 """
 import numpy as np
-## 2x3 Matrise [Matrise A]
-print("Matrise A:")
-#Rad 1
-a11 = float(input("tall for rad: 1, kollonne: 1, = "))
-a12 = float(input("tall for rad: 1. kollonne: 2, = "))
-a13 = float(input("tall for rad: 1, kollonne: 3, = "))
-
-#Rad 2
-a21 = float(input("tall for rad: 2, kollonne: 1, = "))
-a22 = float(input("tall for rad: 2. kollonne: 2, = "))
-a23 = float(input("tall for rad: 2, kollonne: 3, = "))
-
-## 3x2 Matrise [Matrise B]
-print("Matrise B:")
-
-#Rad 1
-b11 = float(input("tall for rad: 1, kollonne: 1, = "))
-b12 = float(input("tall for rad: 1. kollonne: 2, = "))
-
-#Rad 2
-b21 = float(input("tall for rad: 2, kollonne: 1, = "))
-b22 = float(input("tall for rad: 2. kollonne: 2, = "))
-
-#Rad 3
-b31 = float(input("tall for rad: 3, kollonne: 1, = "))
-b32 = float(input("tall for rad: 3. kollonne: 2, = "))
 
 def C11(A_mat, B_mat):
     C11 = B_mat[0][0]*A_mat[0][0] + B_mat[1][0]*A_mat[0][1] + B_mat[2][0]*A_mat[0][2]
@@ -149,19 +104,68 @@ def C22(A_mat, B_mat):
     C22 = B_mat[0][1]*A_mat[1][0] + B_mat[1][1]*A_mat[1][1] + B_mat[2][1]*A_mat[1][2]
     return C22
 
-A_matrise = np.array([[a11, a12, a13], [a21, a22, a23]])
-B_matrise = np.array([[b11, b12], [b21, b22], [b31, b32]])
+# 2x3 Matrise
+Matrise_A = np.random.randint(-10, high=10, size=(2, 3), dtype='l')
+# 3x2 Matrise
+Matrise_B = np.random.randint(-10, high=10, size=(3, 2), dtype='l')
 
-C_matrise_tuple = ([C11(A_matrise, B_matrise), C12(A_matrise, B_matrise)], [C21(A_matrise, B_matrise), C22(A_matrise, B_matrise)])
+
+C_matrise_tuple = ([C11(Matrise_A, Matrise_B), C12(Matrise_A, Matrise_B)], [C21(Matrise_A, Matrise_B), C22(Matrise_A, Matrise_B)])
 C_matrise = np.asarray(C_matrise_tuple)
 
 print("Matrise A multiplisert med B [AB] = matrise C = ", C_matrise)
 """
-
 # Oppg 4)
+"""
+import sympy as sym
 
+# Derivasjon:    
+x = sym.Symbol("x")
+funksjon = ((-x**3)/6) + (1/4)*sym.cos(2*x) + ((sym.E**x)/100) + (3/2)*x**2
+
+funksjon_derivert = sym.diff(funksjon, x)
+#print("Den deriverte til funksjonen = ", funksjon_derivert)
+
+funksjon_dobbelderivert = sym.diff(funksjon_derivert, x)
+#print("Den dobbel-deriverte til funksjonen = ", funksjon_dobbelderivert)
+
+funksjon_trippelderivert = sym.diff(funksjon_dobbelderivert, x)
+#print("Den trippel-deriverte til funksjonen = ", funksjon_trippelderivert)
+
+funksjon_fjerdederivert = sym.diff(funksjon_trippelderivert, x)
+#print("Den fjerde-deriverte til funksjonen = ", funksjon_fjerdederivert)
+
+
+#Brukbare funksjoner i numpy
+
+derivert_to = sym.lambdify(x, funksjon_dobbelderivert)
+derivert_tre = sym.lambdify(x, funksjon_trippelderivert)
+derivert_fire = sym.lambdify(x, funksjon_fjerdederivert)
+
+
+
+def grense(x):
+    # Legger inn funksjonen
+    return (derivert_to(x)*(b-a)**3)/(24*n**2)
+    
+a = 0
+b = 5/2
+n = 20
+m = 10
+xh = 1
+
+for i in range(m):
+    xh = xh - derivert_tre(xh)/derivert_fire(xh)
+    #print(xh)
+
+print("Øvregrense for feil er", grense(xh), "X er", xh)
+"""
 
 # Oppg 5)
+
+"""
+
+# IKKE FERDIG
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -181,3 +185,34 @@ plt.show()
 plt.figure(4)
 plt.plot(x, f, "r*", linewidth = 4)
 plt.show()
+
+
+funksjon = f
+
+# Informasjon om integralet
+n = 46 # antall delintervaller
+a = 0.0 # nedre grense
+b = 15.0 # ovre grense
+
+# Bredden til trapesene
+dx = (b-a)/n
+
+# Forste ledd
+x = x[0][0]
+T = f[0][0]
+
+# Beregner summen av arealene til trapesene
+for i in range (1,n):
+    x = x + dx
+    T = T + 2*funksjon
+
+# Siste ledd
+x = b
+T = T + funksjon
+
+# Ganger summen med bredden av delintervallene 
+T = T*dx/2
+
+# Skriver ut resultatet
+print("Summen av arealene til trapesene hvis n =",n,"er ",T)
+"""
